26680d4b2457fbbce1000944e2e0239aaf128d36
joeldsouza-dev <joeldsouza986@gmail.com>
2025-12-01 14:25:25 +0530

    implemented password encryption by adding Bcrypt to the code

--- src/main/java/com/joel/journals/service/usersService.java
+++ src/main/java/com/joel/journals/service/usersService.java
@@ -4,8 +4,12 @@ import com.joel.journals.entity.users;
 import com.joel.journals.repositary.usersrepos;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.net.PasswordAuthentication;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,7 +19,15 @@ public class usersService {
     @Autowired
     private usersrepos usersrepos;
 
+    private static final PasswordEncoder encoder = new BCryptPasswordEncoder();
+
     public void saveEntry(users userentry) {
+        userentry.setPassword(encoder.encode(userentry.getPassword()));
+        userentry.setRoles(Arrays.asList("USER"));
+        usersrepos.save(userentry);
+    }
+
+    public void saveNewEntry(users userentry) {
         usersrepos.save(userentry);
     }
 
