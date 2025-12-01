fab77c1ecbdb0e2e920ee1a88a72b6b21082f166
joeldsouza-dev <joeldsouza986@gmail.com>
2025-12-01 14:26:11 +0530

    created a public route so that everyone can hit the route without authentication

---
+++ src/main/java/com/joel/journals/controller/publicEntry.java
@@ -0,0 +1,22 @@
+package com.joel.journals.controller;
+
+import com.joel.journals.entity.users;
+import com.joel.journals.service.usersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
+public class publicEntry {
+
+    @Autowired
+    private usersService userentry;
+
+    @PostMapping
+    public void createEntry(@RequestBody users entry) {
+        userentry.saveEntry(entry);
+    }
+}
