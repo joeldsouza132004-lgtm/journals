1b4b72e4e906e9ea67ef0cdf370cb60491aa8644
joeldsouza-dev <joeldsouza986@gmail.com>
2025-12-01 14:22:32 +0530

    enabled transaction management and added a bean for Platform transaction manager

--- src/main/java/com/joel/journals/JournalsApplication.java
+++ src/main/java/com/joel/journals/JournalsApplication.java
@@ -2,12 +2,22 @@ package com.joel.journals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class JournalsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JournalsApplication.class, args);
     }
 
+    @Bean
+    public PlatformTransactionManager add(MongoDatabaseFactory dbFactory){
+        return new MongoTransactionManager(dbFactory);
+    }
 }
