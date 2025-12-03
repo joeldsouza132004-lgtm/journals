36b6d4a8e2530eb4ecc3a9239f8397f6d3c1b634
joeldsouza-dev <joeldsouza986@gmail.com>
2025-12-03 14:54:59 +0530

    added admin route to be authenticated for ADMIN role

--- src/main/java/com/joel/journals/config/SpringSecurity.java
+++ src/main/java/com/joel/journals/config/SpringSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,6 +18,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurity {
     @Autowired
     private UserDetailsService userDetailsService;
@@ -31,10 +33,11 @@ public class SpringSecurity {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth-> auth
                         .requestMatchers("/journal/**","/users/**").authenticated()
+                        .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().permitAll()
-                );
-        http.sessionManagement(session->session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                )
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     return http.build();
     }
     @Bean
