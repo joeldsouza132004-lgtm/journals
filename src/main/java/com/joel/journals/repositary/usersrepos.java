b8b14cea5566e3fecaa6cbef6e93c31eb967ebc5
joeldsouza132004-lgtm <joeldsouza132004@gmail.com>
2025-12-05 12:30:24 +0530

    Delete src/main/java/com/joel/journals/repositary/usersrepos.java
    duplicate file deleted

--- src/main/java/com/joel/journals/repositary/usersrepos.java
+++
@@ -1,9 +0,0 @@
-package com.joel.journals.repositary;
-
-import com.joel.journals.entity.users;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface usersrepos extends MongoRepository<users, ObjectId> {
-    users findByUsername(String username);
-}
