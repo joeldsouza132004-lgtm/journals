abab307da358c7fb99c368ff55842117801fffdb
joeldsouza-dev <joeldsouza986@gmail.com>
2025-12-11 23:16:11 +0530

    allowed every endpoint with/public route to access the url

--- src/main/java/com/joel/journals/config/SpringSecurity.java
+++ src/main/java/com/joel/journals/config/SpringSecurity.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -34,7 +35,10 @@ public class SpringSecurity {
                 .cors(cors -> cors.disable())
                 .csrf(csrf -> csrf.disable())   // required for Postman
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/public/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/public/**").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/public/**").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/public/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form.disable())
