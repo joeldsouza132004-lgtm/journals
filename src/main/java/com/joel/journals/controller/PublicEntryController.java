001f4ecf9eceb854cecda0b0e1ca1dedd23ab38f
joeldsouza-dev <joeldsouza986@gmail.com>
2025-12-12 15:54:09 +0530

    just a check endpoint

--- src/main/java/com/joel/journals/controller/PublicEntryController.java
+++ src/main/java/com/joel/journals/controller/PublicEntryController.java
@@ -1,21 +1,12 @@
 package com.joel.journals.controller;
 
-import com.joel.journals.entity.UserEntry;
-import com.joel.journals.service.usersService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/public")
 public class PublicEntryController {
-
-    @Autowired
-    private usersService userentry;
-
-    @PostMapping
-    public void createEntry(@RequestBody UserEntry entry) {
-        userentry.SaveNewEntry(entry);
-    }
 
     @GetMapping("/ping")
     public String ping() {
