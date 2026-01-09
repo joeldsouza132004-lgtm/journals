184f2436e2fa11425b9a9dd1433638757e4ed58e
joeldsouza-dev <joeldsouza986@gmail.com>
2026-01-09 12:11:38 +0530

    added the message

--- src/main/java/com/joel/journals/controller/PublicEntryController.java
+++ src/main/java/com/joel/journals/controller/PublicEntryController.java
@@ -1,8 +1,10 @@
 package com.joel.journals.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.joel.journals.service.sqs.SqsProducerService;
 
 @RestController
 @RequestMapping("/public")
@@ -11,10 +13,17 @@ public class PublicEntryController {
     @org.springframework.beans.factory.annotation.Autowired
     private com.joel.journals.service.usersService usersService;
 
+    @Autowired
+    private SqsProducerService sqsProducerService;
+
     @org.springframework.web.bind.annotation.PostMapping("/create-user")
     public void createUser(@org.springframework.web.bind.annotation.RequestBody com.joel.journals.entity.UserEntry entry) {
         usersService.SaveNewEntry(entry);
+        sqsProducerService.sendMessage("USER_CREATED:"+entry.getUsername());
     }
+
+
+
 
     @GetMapping("/ping")
     public String ping() {
